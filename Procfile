a502b63d61540817ca8d82b4b661f6694db7939f
https://github.com/Don1k1337/Danil <dan4ik1411@gmail.com>
2019-07-15 12:36:47 +0600

    fixing .gitignore

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Courses.wsgi --log-file -
--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/don1k1337/Desktop/Django/demorest/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
